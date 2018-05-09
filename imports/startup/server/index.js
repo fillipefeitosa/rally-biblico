ae744b83460bc2799ee1ed3e917e1cfd84bace06
Fillipe Feitosa <fillfeitosa@gmail.com>
2018-05-09 21:50:50 +0100

    Grupos de Usuários Adicionados && Rotina de Default Roles no Servidor #3

---
+++ imports/startup/server/index.js
@@ -0,0 +1,2 @@
+import './publish.js';
+import './fixtures.js';
--- server/main.js
+++ server/main.js
@@ -1,27 +1,40 @@
 import { Meteor } from 'meteor/meteor';
+import { Roles } from 'meteor/alanning:roles';
 
 Meteor.startup(() => {
-
-  ServiceConfiguration.configurations.remove({
-    service: "facebook"
-  });
-
-  ServiceConfiguration.configurations.insert({
-    service: "facebook",
-    appId: '836307303236785',
-    secret: '0aef8bd515a0e5ebb26546d01ae3bda8'
-  });
-  // code to run on server at startup
-
-  Accounts.onCreateUser(function (options, user) {
-
-    if (!user.services.facebook) {
-        return user;
+    // Default Roles for Rally Bíblico
+    var roles = Meteor.roles.find({}).count();
+    if (roles > 0) {
+        console.log("Roles Defined. Default Behavior.");
+    } else {
+        Roles.createRole('Convidado');
+        Roles.createRole('Piloto');
+        Roles.createRole('Coordenador');
+        Roles.createRole('Administrador');
     }
-    user.username = user.services.facebook.name;
-    user.emails = [{address: user.services.facebook.email}];
 
-    return user;
+
+    ServiceConfiguration.configurations.remove({
+        service: "facebook"
+    });
+
+    ServiceConfiguration.configurations.insert({
+        service: "facebook",
+        appId: '836307303236785',
+        secret: '0aef8bd515a0e5ebb26546d01ae3bda8'
+    });
+
+    Accounts.onCreateUser(function (options, user) {
+        console.log(user._id);
+        Roles.addUsersToRoles(user._id, ['Convidado']);
+        console.log(user);
+        if (!user.services.facebook) {
+            return user;
+        }
+        user.username = user.services.facebook.name;
+        user.emails = [{address: user.services.facebook.email}];
+
+        return user;
     });
 
 });
