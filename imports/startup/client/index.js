12f4c1202316c6171ffa858698812e27760f0c06
Fillipe Feitosa <fillfeitosa@gmail.com>
2018-11-21 18:08:32 +0000

    Refatorando para usar FlowRouter

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -5,7 +5,9 @@ import './highlighter.js';
 import '../../ui/components/forms.css';
 import '../../ui/components/forms.js';
 
-
+import '../../ui/layouts/body/body.js'
+import '../../ui/layouts/header/header.js'
+import '../../ui/layouts/navbar/navbar.js'
 import '../../ui/layouts/main.js'
 import '../../ui/accounts/signin.js'
 import '../../ui/layouts/piloto.js'
--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,57 +1,126 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { Session } from 'meteor/session';
 
-var PostLogout = function(){
-    Router.go('/');
-};
+// Exposed Routes
 
-AccountsTemplates.configure({
-    defaultLayout: 'main',
-    onLogoutHook: PostLogout
-});
-
-Router.route('/', function(){
-    this.layout('main');
-    this.render('piloto');
-});
-
-
-Router.route('/dadospessoais', function () {
-  this.layout('main');
-  this.render('dadospessoais');
-});
-
-Router.route("/admin", function(){
-    this.layout("main");
-    this.render("admin");
-});
-
-Router.route("/admin/usergroups", function(){
-    this.layout("main");
-    this.render("usergroups");
-});
-
-Router.route("/admin/churches", function(){
-    this.layout("main");
-    this.render("churches");
-});
-
-Router.route("/admin/rallycreate", function(){
-    this.layout("main");
-    this.render("rallycreate");
-});
-
-Router.route("/admin/rallypannel", function(){
-    this.layout("main");
-    this.render("rallypannel");
-});
-
-Router.route("/admin/rally/:_id", function(){
-    this.layout('main');
-    this.render('rallyupdate');
-
-}, {
-    name : 'rally.update',
-    data: function() {
-        return Rally.findOne(this._id);
+exposed = FlowRouter.group({});
+exposed.route('/', {
+    name: 'main',
+    action: function(){
+        BlazeLayout.render('App_body', { navbar:"navbar", top: "header", main: "main", pageContent:"rally"});
     }
-}
-);
+});
+
+// Logged Routes
+loggedIn = FlowRouter.group({
+    triggersEnter: [
+        function(){
+            var route;
+            if(!(Meteor.loggingIn() || Meteor.userId())){
+                route = FlowRouter.current();
+                if (route.route.name !== 'signin') {
+                    // Adicionar Mensagem de Redirecionamento
+                    Session.set('redirectAfterLogin',route.path);
+                }
+                return FlowRouter.go('main');
+            }
+        }
+    ]
+});
+loggedIn.route('/dadospessoais', {
+    name: 'dadospessoais',
+    action: function(){
+        BlazeLayout.render('App_body', {navbar:"navbar", top:"header", main:"main", pageContent:"dadospessoais" });
+    }
+});
+
+var adminRoutes = FlowRouter.group({
+    prefix: '/admin',
+    name: 'admin',
+});
+
+adminRoutes.route('/', {
+    name: 'adminhome',
+    action: function(){
+        BlazeLayout.render('App_body', {navbar:"navbar",top:"header", main:"main", pageContent:"admin" });
+    },
+});
+
+adminRoutes.route('/churches', {
+    name: 'churches',
+    action: function(){
+        BlazeLayout.render('App_body', {navbar:"navbar",top:"header", main:"main", pageContent:"churches"});
+    }
+});
+
+adminRoutes.route('/rallypannel', {
+    name: 'churches',
+    action: function(){
+        BlazeLayout.render('App_body', {navbar:"navbar",top:"header", main:"main", pageContent:"rallypannel"});
+    }
+});
+
+adminRoutes.route('/usergroups', {
+    name: 'churches',
+    action: function(){
+        BlazeLayout.render('App_body', {navbar:"navbar",top:"header", main:"main", pageContent:"usergroups"});
+    }
+});
+
+// LEGACY CODE
+// var PostLogout = function(){
+//     Router.go('/');
+// };
+//
+// AccountsTemplates.configure({
+//     defaultLayout: 'main',
+//     onLogoutHook: PostLogout
+// });
+
+// Router.route('/', function(){
+//     this.layout('main');
+//     this.render('piloto');
+// });
+
+// Router.route('/dadospessoais', function () {
+//   this.layout('main');
+//   this.render('dadospessoais');
+// });
+//
+// Router.route("/admin", function(){
+//     this.layout("main");
+//     this.render("admin");
+// });
+//
+// Router.route("/admin/usergroups", function(){
+//     this.layout("main");
+//     this.render("usergroups");
+// });
+//
+// Router.route("/admin/churches", function(){
+//     this.layout("main");
+//     this.render("churches");
+// });
+//
+// Router.route("/admin/rallycreate", function(){
+//     this.layout("main");
+//     this.render("rallycreate");
+// });
+//
+// Router.route("/admin/rallypannel", function(){
+//     this.layout("main");
+//     this.render("rallypannel");
+// });
+//
+// Router.route("/admin/rally/:_id", function(){
+//     this.layout('main');
+//     this.render('rallyupdate');
+//
+// }, {
+//     name : 'rally.update',
+//     data: function() {
+//         return Rally.findOne(this._id);
+//     }
+// }
+// );
